aa78d656a92631bd5669ca23e9e429470c935950
César Ribeiro <cesarm.ribeiro@hotmail.com>
2024-05-24 18:31:49 -0300

    meu quarto commit neste projeto

--- meu-primeiro-projeto-java/src/minhaClasse.java
+++ meu-primeiro-projeto-java/src/minhaClasse.java
@@ -2,7 +2,7 @@ public class minhaClasse {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Boas Vindas Turma DIO");
+        System.out.print("Boas Vindas Turma DIO");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
