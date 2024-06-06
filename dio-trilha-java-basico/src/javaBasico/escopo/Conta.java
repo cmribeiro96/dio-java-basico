31eabfcdbd35005147a150d7c9e17b3029ffda54
César Ribeiro <cesarm.ribeiro@hotmail.com>
2024-06-05 23:58:05 -0300

    ajustando o projeto conforme Java Doc - Aula 2

--- dio-trilha-java-basico/src/javaBasico/escopo/Conta.java
+++ dio-trilha-java-basico/src/javaBasico/escopo/Conta.java
@@ -12,7 +12,7 @@ public class Conta {
         //disponível em toda classe
         System.out.println(saldo);
         //somente o método sacar conhece esta variavel
-        System.out.println(novoSaldo);
+        //System.out.println(novoSaldo);
 
     }
     public double calcularDividaExponencial(){
